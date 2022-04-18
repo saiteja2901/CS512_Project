fe7c64ca4eceb2900046d1634ade4bde106fab82
saiteja2901 <57471885+saiteja2901@users.noreply.github.com>
2022-04-18 14:37:09 -0500

    Add files via upload

---
+++ fetch_data/places_standard_evaluation_prepare_data.sh
@@ -0,0 +1,56 @@
+# 0. folder preparation
+mkdir -p places_standard_dataset/evaluation/hires/
+mkdir -p places_standard_dataset/evaluation/random_thick_512/
+mkdir -p places_standard_dataset/evaluation/random_thin_512/
+mkdir -p places_standard_dataset/evaluation/random_medium_512/
+mkdir -p places_standard_dataset/evaluation/random_thick_256/
+mkdir -p places_standard_dataset/evaluation/random_thin_256/
+mkdir -p places_standard_dataset/evaluation/random_medium_256/
+
+# 1. sample 30000 new images
+OUT=$(python fetch_data/eval_sampler.py)
+echo ${OUT}
+
+FILELIST=$(cat places_standard_dataset/original/eval_random_files.txt)
+
+OIFS="$IFS"
+IFS=$'\n'
+for i in $FILELIST
+do
+    $(cp "${i}" "places_standard_dataset/evaluation/hires/")
+done
+
+
+# 2. generate all kinds of masks
+
+# all 512
+python bin/gen_mask_dataset.py \
+$(pwd)/configs/data_gen/random_thick_512.yaml \
+places_standard_dataset/evaluation/hires \
+places_standard_dataset/evaluation/random_thick_512/
+
+python bin/gen_mask_dataset.py \
+$(pwd)/configs/data_gen/random_thin_512.yaml \
+places_standard_dataset/evaluation/hires \
+places_standard_dataset/evaluation/random_thin_512/
+
+python bin/gen_mask_dataset.py \
+$(pwd)/configs/data_gen/random_medium_512.yaml \
+places_standard_dataset/evaluation/hires \
+places_standard_dataset/evaluation/random_medium_512/
+
+python bin/gen_mask_dataset.py \
+$(pwd)/configs/data_gen/random_thick_256.yaml \
+places_standard_dataset/evaluation/hires \
+places_standard_dataset/evaluation/random_thick_256/
+
+python bin/gen_mask_dataset.py \
+$(pwd)/configs/data_gen/random_thin_256.yaml \
+places_standard_dataset/evaluation/hires \
+places_standard_dataset/evaluation/random_thin_256/
+
+python bin/gen_mask_dataset.py \
+$(pwd)/configs/data_gen/random_medium_256.yaml \
+places_standard_dataset/evaluation/hires \
+places_standard_dataset/evaluation/random_medium_256/
+IFS="$OIFS"
---
+++ fetch_data/places_standard_test_val_prepare.sh
@@ -0,0 +1,5 @@
+mkdir -p places_standard_dataset/original/test/
+tar -xvf test_256.tar -C places_standard_dataset/original/test/
+
+mkdir -p places_standard_dataset/original/val/
+tar -xvf val_256.tar -C places_standard_dataset/original/val/
